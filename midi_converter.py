38e3498c03d9bc9bf4ef218445476b55cf10cadd
cwlebgarcia <calebf3g4@gmail.com>
2025-02-15 19:31:39 -0700

    converter progress

--- midi_converter.py
+++ midi_converter.py
@@ -1,19 +1,41 @@
 import os
 import mido
+import math
 
-mid = MidiFile(FileName, clip=True)
+FileName = 'overworld.mid'
+mid = mido.MidiFile(FileName, clip=True)
+# print(mid)
 
-notes = []
+# for track in mid.tracks:
+#     print(track)
 
-num_to_note = {}
+# for msg in mid.tracks[0]:
+#     print(msg)
 
-for track in mido.tracks(mid):
-    for msg in track:
-        if msg[0] == 'note_on':
-            notes[msg] =  [int(num) for num in msg[2]] 
+pitches = []
+lengths = []
+tempo = 0
+
+for msg in mid.tracks[1]:
+    if msg.is_meta: # Print metadata
+        print(msg) 
+        tempo = msg.tempo
+    if msg.type == 'note_on': # If there is a note, convert it to a frequency and append to the list of notes, else append a zero
+        pitches.append(math.ceil(440 * 2 ** ((msg.note - 69) / 12)))
+    else:
+        pitches.append(0)   
+
+    # lengths.append(mido.tick2second(msg.time) / 10 ** 3) #mido.tick2second(tick, ticks_per_beat, tempo)
+# print(lengths)
+print(tempo)
+
+if len(pitches) != len(lengths):
+    print("Warnig: len(pitches) != len(lengths)")
+
 
 # Message('note_on', channel=0, note=60, velocity=64, time=0)
 # Board buzzers allow from C3-B6, corresponding to 48-95 in Midi
 # Note values, progressing in half steps
 
-# 440 * 2 ^ (n-69) / 12
+# 440 * 2 ** (n-69) / 12
+# A tick is 8 ms in MIDI
